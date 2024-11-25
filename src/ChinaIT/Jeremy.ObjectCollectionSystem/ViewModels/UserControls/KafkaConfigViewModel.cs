7819952e3352ab885cb4631596e1894a681ed531
JeremyWu <1280442096@qq.com>
2024-11-25 10:22:27 +0800

    feat: add kafka config view model

---
+++ src/ChinaIT/Jeremy.ObjectCollectionSystem/ViewModels/UserControls/KafkaConfigViewModel.cs
@@ -0,0 +1,127 @@
+﻿using HandyControl.Data;
+using Jeremy.ObjectCollectionSystem.Domains;
+using Jeremy.ObjectCollectionSystem.Models;
+using Jeremy.ObjectCollectionSystem.Services;
+
+namespace Jeremy.ObjectCollectionSystem.ViewModels.UserControls;
+
+public class KafkaConfigViewModel : ObservableRecipient
+{
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    public KafkaConfigViewModel()
+    {
+        InitAsync();
+    }
+
+    #region 声明通知属性
+    private TbKafkaConfig _TbKafkaConfig;
+    /// <summary>
+    /// Kafka 信息配置对象
+    /// </summary>
+    public TbKafkaConfig TbKafkaConfig
+    {
+        get => _TbKafkaConfig;
+        set => SetProperty(ref _TbKafkaConfig, value);
+    }
+
+    #endregion
+
+    #region 声明事件并实现
+    /// <summary>
+    /// 取消事件声明
+    /// </summary>
+    public RelayCommand<EventArgs> CancelCommand => new(Cancel);
+    /// <summary>
+    /// 取消事件实现
+    /// </summary>
+    /// <param name="args"></param>
+    private void Cancel(EventArgs args)
+    {
+        InitAsync();
+        Growl.Success(new GrowlInfo()
+        {
+            Message = "取消成功！",
+            WaitTime = 1
+        });
+    }
+    /// <summary>
+    /// 保存事件声明
+    /// </summary>
+    public RelayCommand<EventArgs> SaveCommand => new(Save);
+    /// <summary>
+    /// 保存事件实现
+    /// </summary>
+    /// <param name="args"></param>
+    private void Save(EventArgs args)
+    {
+        try
+        {
+            // 数据校验
+            if (string.IsNullOrWhiteSpace(TbKafkaConfig.BootstrapServers))
+            {
+                Growl.Warning(new GrowlInfo()
+                {
+                    Message = $"集群地址不能为空，请确认！",
+                    WaitTime = 1
+                });
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbKafkaConfig.Topic))
+            {
+                Growl.Warning(new GrowlInfo()
+                {
+                    Message = $"Topic 名称不能为空，请确认！",
+                    WaitTime = 1
+                });
+                return;
+            }
+
+            TbKafkaConfig.UpdateBy = "Administrator";
+            TbKafkaConfig.UpdateTime = DateTime.Now;
+            // 保存数据库信息
+            _ = KafkaConfigService.Put(TbKafkaConfig);
+            GlobalInfo.BootstrapServers = TbKafkaConfig.BootstrapServers;
+            GlobalInfo.Topic = TbKafkaConfig.Topic;
+            // 保存 
+            Growl.Success(new GrowlInfo()
+            {
+                Message = "保存成功！",
+                WaitTime = 1
+            });
+        }
+        catch (Exception ex)
+        {
+            Growl.Error(new GrowlInfo()
+            {
+                Message = $"保存失败，错误信息 - {ex.Message}",
+                WaitTime = 1
+            });
+        }
+    }
+    #endregion
+
+    #region 私有函数
+    /// <summary>
+    /// 初始化函数
+    /// </summary>
+    private async Task InitAsync()
+    {
+        TbKafkaConfig = new();
+        try
+        {
+            TbKafkaConfig = await KafkaConfigService.GetAsync();
+        }
+        catch (Exception ex)
+        {
+            Growl.Error(new GrowlInfo()
+            {
+                Message = $"获取 Kafka 配置信息失败，错误信息 - {ex.Message}",
+                WaitTime = 1
+            });
+        }
+    }
+    #endregion
+
+}
--- src/ChinaIT/Jeremy.ObjectCollectionSystem/Views/UserControls/KafkaConfigUserControl.xaml.cs
+++ src/ChinaIT/Jeremy.ObjectCollectionSystem/Views/UserControls/KafkaConfigUserControl.xaml.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using Jeremy.ObjectCollectionSystem.ViewModels.UserControls;
 
-namespace Jeremy.ObjectCollectionSystem.Views.UserControls
+namespace Jeremy.ObjectCollectionSystem.Views.UserControls;
+
+/// <summary>
+/// Interaction logic for KafkaConfigUserControl.xaml
+/// </summary>
+public partial class KafkaConfigUserControl : UserControl
 {
-    /// <summary>
-    /// Interaction logic for KafkaConfigUserControl.xaml
-    /// </summary>
-    public partial class KafkaConfigUserControl : UserControl
+    public KafkaConfigUserControl()
     {
-        public KafkaConfigUserControl()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        DataContext = new KafkaConfigViewModel();
     }
 }
