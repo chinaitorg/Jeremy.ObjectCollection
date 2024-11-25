52159b6e006d08eb327555818133665299af3fa6
JeremyWu <1280442096@qq.com>
2024-11-25 10:28:56 +0800

    feat: open page config of kafka

--- src/ChinaIT/Jeremy.ObjectCollectionSystem/ViewModels/Windows/MainWindowViewModel.cs
+++ src/ChinaIT/Jeremy.ObjectCollectionSystem/ViewModels/Windows/MainWindowViewModel.cs
@@ -212,7 +212,7 @@ public class MainWindowViewModel : ObservableRecipient
             TabItem tabItem = new()
             {
                 Header = "Kafka 配置",
-                //Content = new KafkaConfigUserControl()
+                Content = new KafkaConfigUserControl()
             };
             tabItem.SetValue(IconElement.HeightProperty, 16.0);
             tabItem.SetValue(IconElement.WidthProperty, 16.0);
