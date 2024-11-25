e71f93dfaff339d5f81ffdbbc1446ff6d3f8bfe5
JeremyWu <1280442096@qq.com>
2024-11-25 11:50:45 +0800

    feat: add log view page

---
+++ src/ChinaIT/Jeremy.ObjectCollectionSystem/Views/UserControls/LogViewUserControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Jeremy.ObjectCollectionSystem.Views.UserControls;
+
+/// <summary>
+/// Interaction logic for LogViewUserControl.xaml
+/// </summary>
+public partial class LogViewUserControl : UserControl
+{
+    public LogViewUserControl()
+    {
+        InitializeComponent();
+    }
+}
